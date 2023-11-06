c896be3c62f396e12e12c2e5ceef4f83e3505e52
Emil ventura <m17-0137@ipl.edu.do>
2023-11-06 17:52:55 -0400

    fix: correcciones

--- EstructuraDatos2.cpp
+++ EstructuraDatos2.cpp
@@ -18,32 +18,7 @@ bool EsNumeroValido(string str) {
 		return false; // Si la conversión falla, no es un número decimal válido.
 	}
 }
-int* obtenerprimerasubdivision(double num) {
-	int* arreglo = new int[7]; // Asigna dinámicamente un arreglo de 7 enteros
 
-	// Obtener la parte entera del número
-	int parte_entera = static_cast<int>(num);
-
-	// Obtener la parte decimal del número
-	double parte_decimal = num - parte_entera;
-
-	// Calcular la subdivisión
-	arreglo[0] = (parte_entera / 100000000); // Centenas de millones
-	arreglo[1] = (parte_entera % 100000000) / 1000000; // Decenas y unidades de millón
-	arreglo[2] = ((parte_entera % 1000000) / 100000); // Centenas de miles
-	arreglo[3] = (parte_entera % 100000) / 1000; // Decenas y unidades de miles
-	arreglo[4] = (parte_entera / 100) % 10; // Centenas
-	arreglo[5] = parte_entera % 100; // Decenas y unidades
-	arreglo[6] = parte_decimal * 100; // Decimales
-	return arreglo;
-}
-
-string CentenasEnTexto(int centena)
-{
-	string unidades[] = { "Cero", "Cien", "Doscientos", "Trescientos", "Cuatrocientos", "Quinientos", "Seiscientos", "Setecientos", "Ochocientos", "Novecientos" };
-
-	return unidades[centena];
-}
 
 string DecenasUnidadesEnTexto(int numero) {
 	string unidades[] = { "Cero", "Uno", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "Ocho", "Nueve" };
@@ -80,72 +55,11 @@ string DecenasUnidadesEnTexto(int numero) {
 			texto = decenas[decena] + " y " + unidades[unidad];
 		}
 	}
-
 	return texto;
 }
 
-void finality(int* arr)
-{
-	string texto;
-	string nomenclatura[] = { " Millones ", " mil "," Pesos dominicanos con ", " Centimos " };
 
-	for (size_t i = 0; i < 7; i++)
-	{
-		if (arr[i] == 0 && i != 6)
-		{
-			continue;
-		}
-
-		if (i % 2 == 0 && i < 6)
-		{
-			texto += CentenasEnTexto(arr[i]);
-			if (arr[i] == 1 && arr[i + 1] != 0)
-			{
-				texto += "to";
-			}
-
-		}
-		else
-		{
-			texto += DecenasUnidadesEnTexto(arr[i]);
-		}
-
-		if (i == 1)
-		{
-			texto += nomenclatura[0];
-		}
-		else if (i == 3)
-		{
-			texto += nomenclatura[1];
-
-		}
-		else if (i == 5)
-		{
-			texto += nomenclatura[2];
-		}
-
-		else if (i == 6)
-		{
-			texto += nomenclatura[3] + " ";
-
-		}
-		else
-		{
-			texto += " ";
-
-		}
-
-
-
-	}
-
-	cout << texto;
-	cout << endl;
-
-}
-
-
-string numeroATextoRecursivo(size_t entradaNumero) {
+string numeroATextoRecursivo(int entradaNumero) {
 
 	size_t Auxiliar, cien = 100, mil = 1000, millon = 1000000;
 	string textoGlobal = "";
@@ -197,44 +111,10 @@ string numeroATextoRecursivo(size_t entradaNumero) {
 	return textoGlobal;
 }
 
-void TransformarNumero() {
-	double numero;
-	string entrada;
-	bool invalido = true;
-
-	while (invalido) {
-		cout << "Ingresa un número válido: ";
-		cin >> entrada;
-
-		if (EsNumeroValido(entrada)) {
-			numero = stod(entrada);
-			break;
-		}
-		else {
-			cout << "No has ingresado un número válido. Inténtalo de nuevo." << endl;
-		}
-	}
-
-
-	int* resultado = obtenerprimerasubdivision(numero);
-
-
-	for (size_t i = 0; i < 7; i++)
-	{
-		cout << resultado[i] << " ";
-	}
-
-	cout << endl;
-
-	finality(resultado);
-
-	delete[] resultado;
-}
-
 int main() {
 
 	bool continuar = true;
-	size_t numero;
+	int numero;
 	string texto;
 	while (continuar)
 	{
@@ -253,7 +133,11 @@ int main() {
 
 		}
 		//Agregar centimos:
+		/*
+		* int parte_entera = static_cast<int>(num);
 
+			double parte_decimal = num - parte_entera;
+		*/
 
 		cout << endl << texto<< endl;
 	}
